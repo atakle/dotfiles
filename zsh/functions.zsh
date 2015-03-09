0490d13f40e6f65c78a2d060c8ff2ebb1b38856a
Anne Takle <annetak@stud.ntnu.no>
2015-03-09 15:36:07 +0100

    zsh: Check if programs are in PATH rather than in /usr/bin.

--- zsh/functions.zsh
+++ zsh/functions.zsh
@@ -1,12 +1,12 @@
 # Create a new screen window with $PWD as the working directory
-if [ -x "/usr/bin/screen" ]; then
+if whence -p screen > /dev/null; then
     function ds {
         screen zsh -c "cd \"$PWD\" && exec $SHELL --login"
     }
 fi
 
 # Open an address in (a new tab in) konqueror
-if [ -x "/usr/bin/kfmclient" ]; then
+if whence -p kfmclient > /dev/null; then
     function kq {
         if [ -z "$*" ]; then
             kfmclient openURL .
