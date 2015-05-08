c59254c0dcedd2d87309b1529923ca87818f302d
Anne Takle <annetak@stud.ntnu.no>
2015-05-09 01:08:55 +0200

    zsh: Add battery status to the prompt.

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -7,8 +7,9 @@ if [ -z "$zsh_prompt_colour" ]; then
 fi
 
 # Add some additional functionality.
-source "$ZSH_DIR/vi-mode.zsh"
+source "$ZSH_DIR/battery.zsh"
 source "$ZSH_DIR/vcs.zsh"
+source "$ZSH_DIR/vi-mode.zsh"
 
 # Calling vi_mode_prompt_info will print the value of MODE_INDICATOR when in
 # normal mode. Use this to make the prompt character red.
@@ -30,7 +31,8 @@ local prt_char="\$(vi_mode_prompt_info)%#%f"
 
 PROMPT="[$prt_colour$prt_userhost $prt_dir]$prt_char "
 
-local prt_vcs="\${vcs_info_msg_0_}" # Version control info.
+local prt_bat="\$(prompt_battery)"  # Battery indicator.
 local prt_jobs="%(1j. {%j}.)"       # Number of background jobs, if any.
+local prt_vcs="\${vcs_info_msg_0_}" # Version control info.
 
-RPOMPT="$prt_vcs$prt_jobs"
+RPROMPT="${prt_vcs}${prt_bat}${prt_jobs}"
