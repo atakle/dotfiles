780f302b139ef3b8dc1685dcbcb72f765ef36ea5
Anne Takle <annetak@stud.ntnu.no>
2015-05-08 18:27:50 +0200

    zsh: Add vi mode indication to the prompt.

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -1,7 +1,13 @@
+# Enable parameter and command expansion in prompts.
+setopt prompt_subst
+
 # Allow each host to use a distinct colour
 if [ -z "$zsh_prompt_colour" ]; then
     zsh_prompt_colour="green"
 fi
+
+# Load vi-mode functionality.
+source "$ZSH_DIR/vi-mode.zsh"
 
 # Split up the prompt definition.
 local prompt_userhost="%B%(!.%F{red}.%F{$zsh_prompt_colour})%n@%m%b%f"
@@ -9,9 +15,9 @@ local prompt_dir="%B%F{blue}%3~%b%f"
 local prompt_char="\$(vi_mode_prompt_info)%#%f"
 
 # Vi normal mode changes the colour of $prompt_char.
-# MODE_INDICATOR="%F{red}"
+MODE_INDICATOR="%F{red}"
 
-PROMPT="[$prompt_userhost $prompt_dir]%# "
+PROMPT="[$prompt_userhost $prompt_dir]$prompt_char "
 
 # ZSH_THEME_GIT_PROMPT_PREFIX="("
 # ZSH_THEME_GIT_PROMPT_SUFFIX=")"
@@ -19,4 +25,3 @@ PROMPT="[$prompt_userhost $prompt_dir]%# "
 # ZSH_THEME_GIT_PROMPT_CLEAN=" %B%F{green}✔%b%f"
 
 # RPROMPT="\$(git_prompt_info)\${\$(battery_pct_prompt)/\\[/ [}%(1j. {%j}.)"
-RPROMPT="%(1j. {%j}.)"
