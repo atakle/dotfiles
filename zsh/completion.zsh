d5c752fca021340e9713676f1319c9219403f520
Anne Takle <annetak@stud.ntnu.no>
2015-05-08 16:33:36 +0200

    zsh: completion settings.

--- zsh/completion.zsh
+++ zsh/completion.zsh
@@ -1,7 +1,14 @@
+# Start the completion system.
+autoload -U compinit
+compinit
+
+setopt complete_in_word
+setopt auto_menu
+
 # First try case sensitive matching. If that doesn't match anything, try
 # case insensitive matching.
 zstyle ':completion:*' matcher-list \
     ''                              \
-    'm:{a-zA-Z}={A-Za-z}'           \
     'r:|[._-]=* r:|=*'              \
-    'l:|=* r:|=*'
+    'l:|=* r:|=*'                   \
+    'm:{a-zA-Z}={A-Za-z}'
