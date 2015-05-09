6517aa1af436ae7abf47acfb89ad58e5d5c3dfa8
Anne Takle <annetak@stud.ntnu.no>
2015-05-09 20:13:36 +0200

    zsh: Adjusted the history settings.

--- zsh/history.zsh
+++ zsh/history.zsh
@@ -2,8 +2,9 @@ if [ -z "$HISTFILE" ]; then
     HISTFILE=$ZSH_DIR/zsh_history
 fi
 
-HISTSIZE=50000
+HISTSIZE=50000 # Maximum history size.
+SAVEHIST=10000 # Maximun number of entries to add from a single session.
 
-setopt append_history
-setopt extended_history
-setopt hist_ignore_dups
+setopt append_history         # Don't truncate an existing file.
+setopt hist_expire_dups_first # Replace duplicate commands first.
+setopt hist_ignore_dups       # Don't add consecutive duplicate commands.
