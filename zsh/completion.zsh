26b8001d6c6bd090f97ecfac7decc78e884a10df
Anne Takle <annetak@stud.ntnu.no>
2015-05-12 16:22:45 +0200

    zsh: Made some changes to the completion options.

--- zsh/completion.zsh
+++ zsh/completion.zsh
@@ -1,14 +1,31 @@
+setopt complete_in_word # Don't move the cursor to the end of the word before
+                        # completing.
+
+setopt auto_menu # Use a menu after the second request for completion.
+
+# Which completers to try, in order.
+zstyle ':completion:*' completer                                      \
+    _complete    `: Normal completion.`                               \
+    _match       `: Match using the patterns in 'matcher-list'`       \
+    _approximate `: Similar to _complete, but with error correction.`
+
+# The matches to try with the _match completer.
+zstyle ':completion:*' matcher-list \
+     'r:|[._-]=** r:|=**'           \
+     'l:|=*       r:|=*'            \
+     'm:{[:lower:]}={[:upper:]}'    \
+    '+m:{[:lower:][:upper:]}={[:upper:][:lower:]}'
+
+# Try running _match without an * at the cursor position first, and add the *
+# if that doesn't work.
+zstyle ':completion:*' match-original both
+
+# Allow at most two errors with the _approximate completer.
+zstyle ':completion:*' max-errors 2
+
+# Start menu selection if the matches list doesn't fit on the screen.
+zstyle ':completion:*' menu select=long
+
 # Start the completion system.
 autoload -U compinit
 compinit -d "$ZSH_DIR/zcompdump"
-
-setopt complete_in_word
-setopt auto_menu
-
-# First try case sensitive matching. If that doesn't match anything, try
-# case insensitive matching.
-zstyle ':completion:*' matcher-list \
-    ''                              \
-    'r:|[._-]=* r:|=*'              \
-    'l:|=* r:|=*'                   \
-    'm:{a-zA-Z}={A-Za-z}'
