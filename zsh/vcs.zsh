4dad8247cf118a9cf989d52d2f7a1792ea04d5c7
Anne Takle <annetak@stud.ntnu.no>
2015-05-14 14:44:07 +0200

    zsh: Commits ahead and behind for git.

--- zsh/vcs.zsh
+++ zsh/vcs.zsh
@@ -22,24 +22,30 @@ function +vi-git-countformat() {
     local prt_git_m=" %{%F{yellow}%}x0%{%f%}"
     local prt_git_c=" %{%B%F{red}%}!0%{%b%f%}"
     local prt_git_u=" %{%F{magenta}%}?0%{%f%}"
+    local prt_git_a=" %{%F{green}%}→0%{%f%}"
+    local prt_git_b=" %{%F{red}%}←0%{%f%}"
 
     local -a changes
     # Parse the git status output and assign to `changes' an array of four
     # elements, containing the number of staged, modified, conflicted, and
     # untracked files, respectively.
     changes=($(
-        git status -s --porcelain |
-        awk '/^A/    {a+=1}
-             /^M/    {a+=1}
-             /^D/    {a+=1}
+        git status -bs --porcelain |
+        awk '/^##.*ahead/  {a=$4}
+             /^##.*behind/ {b=$4}
+             /^##.*,/  {a=$4; b=$6}
+             /^A/    {s+=1}
+             /^M/    {s+=1}
+             /^D/    {s+=1}
              /^.M/   {m+=1}
              /^.A/   {m+=1}
              /^.D/   {m+=1}
              /^U./   {c+=1}
              /^.U/   {c+=1}
              /^.?\?/ {u+=1}
-             END { print a+0, m+0, c+0, u+0 }')) # Add 0 to prevent empty
-                                                 # strings.
+             # Add 0 to make nonempty and coerce to numbers.
+             END { print s+0, m+0, c+0, u+0, a+0, b+0}'))
+    hook_com[staged]=""
     hook_com[unstaged]=""
     test ${changes[1]} != "0" &&
         hook_com[staged]="${prt_git_s/0/${changes[1]}}"
@@ -54,6 +60,12 @@ function +vi-git-countformat() {
         # exploit the `staged' variable to indicate a clean repository.
         hook_com[staged]=" %{%B%F{green}%}✔%{%b%f%}"
     fi
+
+    [[ ${changes[5]} != "0" ]] &&
+        hook_com[unstaged]+=${prt_git_a/0/${changes[5]}}
+    [[ ${changes[6]} != "0" ]] &&
+        hook_com[unstaged]+=${prt_git_b/0/${changes[6]}}
+
 }
 
 # Run the countformat function before returning the message.
