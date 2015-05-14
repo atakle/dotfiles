2b6e806a647ae92730ebe4937ec71ac2f1fc7a7a
Anne Takle <annetak@stud.ntnu.no>
2015-05-14 14:58:46 +0200

    zsh: Some formatting changes.

--- zsh/vcs.zsh
+++ zsh/vcs.zsh
@@ -15,45 +15,42 @@ zstyle ':vcs_info:*' get-unapplied true
 # Modify the %c and %u strings to display the number of staged changes,
 # unstaged changes and untracked files.
 function +vi-git-countformat() {
-    # Display formats for staged, modified, conflicted and untracked files,
-    # respectively. The `0' will be replaced by the corresponding number of
-    # files.
-    local prt_git_s=" %{%F{green}%}+0%{%f%}"
-    local prt_git_m=" %{%F{yellow}%}x0%{%f%}"
-    local prt_git_c=" %{%B%F{red}%}!0%{%b%f%}"
-    local prt_git_u=" %{%F{magenta}%}?0%{%f%}"
-    local prt_git_a=" %{%F{green}%}→0%{%f%}"
-    local prt_git_b=" %{%F{red}%}←0%{%f%}"
+    # Display formats for the git status. The `0' will be replaced by the
+    # corresponding number of files or commits.
+    local prt_git_s=" %{%F{green}%}+0%{%f%}"   # Staged files.
+    local prt_git_m=" %{%F{yellow}%}x0%{%f%}"  # Unstaged modifications.
+    local prt_git_c=" %{%B%F{red}%}!0%{%b%f%}" # Files with conflicts.
+    local prt_git_u=" %{%F{magenta}%}?0%{%f%}" # Untracked files.
+    local prt_git_a=" %{%F{green}%}→0%{%f%}"   # Commits ahead of origin.
+    local prt_git_b=" %{%F{red}%}←0%{%f%}"     # Commits behind origin.
 
     local -a changes
-    # Parse the git status output and assign to `changes' an array of four
-    # elements, containing the number of staged, modified, conflicted, and
-    # untracked files, respectively.
+    # Parse the git status output and assign the result to `changes'.
     changes=($(
         git status -bs --porcelain |
-        awk '/^##.*ahead/  {a=$4}
-             /^##.*behind/ {b=$4}
-             /^##.*,/  {a=$4; b=$6}
-             /^A/    {s+=1}
-             /^M/    {s+=1}
-             /^D/    {s+=1}
-             /^.M/   {m+=1}
-             /^.A/   {m+=1}
-             /^.D/   {m+=1}
-             /^U./   {c+=1}
-             /^.U/   {c+=1}
-             /^.?\?/ {u+=1}
+        awk '/^##.*ahead/  {a=$4}        # a: commits ahead
+             /^##.*behind/ {b=$4}        # b: commits behind
+             /^##.*,/      {a=$4; b=$6}  # s: staged files
+             /^A/          {s++}         # m: modifications
+             /^M/          {s++}         # c: conflicts
+             /^D/          {s++}         # u: untracked files
+             /^.M/         {m++}
+             /^.A/         {m++}
+             /^.D/         {m++}
+             /^U./         {c++}
+             /^.U/         {c++}
+             /^.?\?/       {u++}
              # Add 0 to make nonempty and coerce to numbers.
              END { print s+0, m+0, c+0, u+0, a+0, b+0}'))
     hook_com[staged]=""
     hook_com[unstaged]=""
-    test ${changes[1]} != "0" &&
+    [[ ${changes[1]} != "0" ]] &&
         hook_com[staged]="${prt_git_s/0/${changes[1]}}"
-    test ${changes[2]} != "0" &&
+    [[ ${changes[2]} != "0" ]] &&
         hook_com[unstaged]+="${prt_git_m/0/${changes[2]}}"
-    test ${changes[3]} != "0" &&
+    [[ ${changes[3]} != "0" ]] &&
         hook_com[unstaged]+="${prt_git_c/0/${changes[3]}}"
-    test ${changes[4]} != "0" &&
+    [[ ${changes[4]} != "0" ]] &&
         hook_com[unstaged]+="${prt_git_u/0/${changes[4]}}"
 
     if [ -z "${hook_com[unstaged]}${hook_com[staged]}" ]; then
