fb1c79280f20ca91c425edc76261ffc197ff3621
Anne Takle <anne@takle.priv.no>
2023-01-05 19:23:16 +0100

    zsh: Use ~/.local/bin instead of ~/bin

--- zsh/env.zsh
+++ zsh/env.zsh
@@ -4,10 +4,7 @@ export VISUAL="/usr/bin/vim"
 LS_COLORS='rs=0:di=01;34:ln=01;36:mh=00:pi=40;33:so=01;35:do=01;35:bd=40;33;01:cd=40;33;01:or=40;31;01:su=37;41:sg=30;43:ca=30;41:tw=30;42:ow=34;42:st=37;44:ex=01;32:';
 export LS_COLORS
 
-# Add the user's bin folder to PATH if it exists and is not already there.
-if [ -d "$HOME/bin" ]; then
-    case :$PATH: in
-        *:$HOME/bin:*) ;; # Already present
-        *) PATH="$HOME/bin:$PATH"
-    esac
+if [ -d "$HOME/.local/bin" ]; then
+    path=("$HOME/.local/bin" $path)
+    typeset -U path PATH
 fi
