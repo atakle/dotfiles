e82adffd6ea9c965fde18c91bbc1524c58e0de02
Anne Takle <anne@takle.priv.no>
2025-02-06 19:33:29 +0100

    zsh: redo the prompt code
    The end result is still an enormous chain of zsh substitution sequences,
    but it's defined piecewise and described, so _maybe_ it's still somewhat
    readable if you don't know zshexpn(1) by heart. Maybe.
    
    I've ditched the multiline prompt for now; the current directory is
    shortened in the middle once it starts getting long.
    
    I've also moved all the logic out of the precmd() function; the prompt
    string is defined once at startup, instead of getting redefined after
    every command.

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -1,104 +1,90 @@
 # Enable parameter and command expansion in prompts.
 setopt prompt_subst
 
-# Allow each host to use a distinct colour.
-if [ -z "$zsh_prompt_colour" ]; then
-    # Green by default.
-    zsh_prompt_colour="%B%F{green}"
-fi
-
 # Add some additional functionality.
 source "$ZSH_DIR/battery.zsh"
 source "$ZSH_DIR/vcs.zsh"
 source "$ZSH_DIR/vi-mode.zsh"
 
 # Calling `vi_mode_prompt_info' will print the value of MODE_INDICATOR when in
-# normal mode. Use this to make the prompt character red.
+# normal mode. This is used to make the shell state character red.
 MODE_INDICATOR="%F{red}"
 
-# Components that don't need to be updated:
+# Construct the left part of the prompt. The definition is split into pieces for
+# legibility.
+_left_prompt() {
+    # Working directory
+    local working_dir_full="%~"
+    # Showing only the first two (%-2~) and the last two (%2~) segments. We're
+    # also removing the bold colouring from the '...' in between, so it stands
+    # out a little.
+    local working_dir_truncated="%-2~/%{%b%}...%{%B%}/%2~"
+    # Use truncated path if it's five or more segments long:
+    local working_dir_txt="%(5~|$working_dir_truncated|$working_dir_full)"
+    local working_dir="%{%B%F{blue}%}${working_dir_txt}%{%b%f%}"
 
-local prt_user="%n@%m" # The user@host string.
-local prt_dir="%~"     # Current working directory.
-local prt_char="%#"    # The final character, % for users and # for root.
+    # Username and hostname
+    local user_host_txt="%n@%m"
+    # Displayed in red if running as root:
+    local colour_root="%{%B%F{red}%}"
+    # Or in whatever colour is defined by 'zsh_prompt_colour' if running as a
+    # regular user; green by default:
+    local colour_user="%{\${zsh_prompt_colour:-%B%F{green}}%}"
+    # Colour depends on whether the shell is running with privileges:
+    local user_host="%(!|$colour_root|$colour_user)$user_host_txt%{%b%f%}"
 
-# Versions of the above with colour added. These are defined separately,
-# because they make it hard to compute the length of the prompt.
-local col_user="%(!.%{%B%F{red}%}.%{$zsh_prompt_colour%})$prt_user%{%b%f%}"
-local col_dir="%{%B%F{blue}%}$prt_dir%{%f%b%}"
-local col_char="%{\$(vi_mode_prompt_info)%}$prt_char%{%b%f%}"
+    # Return status of the previous command
+    local return_status_txt="%?"
+    local return_status_colour="(%{%F{red}%}$return_status_txt%{%f%}%) "
+     # Show only if nonzero:
+    local return_status="%(0?||$return_status_colour)"
 
-local prt_jobs="%(1j. {%j}.)" # Number of background jobs, if any.
+    # Shell state (the final character), displayed as '#' if running with
+    # privileges, and '%' otherwise:
+    local shell_state_txt="%#"
+    # The vi mode is indicated by changing the colour of the shell state
+    # character, using the sequence defined in 'MODE_INDICATOR' above.
+    local shell_state="%{\$(vi_mode_prompt_info)%}$shell_state_txt%{%b%f%}"
 
-# Return status of the last command in red, if it was nonzero.
-local col_ret="%(0?..(%{%F{red}%}%?%{%f%}%) )"
-
-# Expression that matches all non-visible character sequences.
-local zero='%([BSUbfksu]|([FB]|){*})'
-
-# Prevent $prt_jobs from going blank when a child process ends, while still
-# taking up space.
-TRAPCHLD() {
-    [[ -o notify ]] && zle && { zle reset-prompt; zle -R }
+    # Full prompt: '(1) [username@hostname /usr/share/example]% '
+    echo "${return_status}[${user_host} ${working_dir}]${shell_state} "
 }
 
-# The precmd() function is called every time before the prompt appears.
+# Construct the right part of the prompt.
+_right_prompt() {
+    # Basename (tail) of the Python virtualenv directory:
+    local python_virtualenv_txt="(\${VIRTUAL_ENV:t})"
+    # Or an empty string if no virtualenv is in use:
+    local python_virtualenv="\${VIRTUAL_ENV:+$python_virtualenv_txt}"
+
+    # Version control information is read from the 'vcs_info_msg_0_' variable.
+    # The display format is defined in 'vcs.zsh'.
+    local version_control="\${vcs_info_msg_0_}"
+
+    # Battery status display is defined in 'battery.zsh'.
+    local battery="\$(prompt_battery)"
+
+    # Number of background jobs
+    background_jobs_txt=" {%j}"
+    # Display only if at least one:
+    background_jobs="%(1j|$background_jobs_txt|)"
+
+    echo "${python_virtualenv}${version_control}${battery}${background_jobs}"
+}
+
+PROMPT="$(_left_prompt)"
+RPROMPT="$(_right_prompt)"
+
+# These are no longer needed:
+unfunction _left_prompt _right_prompt
+
+# Update version control info before rendering the prompt.
 precmd() {
-    vcs_info # Update VCS information. # This sets the value of
-             # $vcs_info_msg_0_.
-    local col_vcs="${vcs_info_msg_0_}"
+    vcs_info
+}
 
-    # Update the battery indicator.
-    local col_bat="$(prompt_battery)"
-
-    # Python virtualenv
-    local prt_venv="$(echo "$VIRTUAL_ENV" \
-      | xargs -r basename \
-      | xargs -r printf " (%s)")"
-
-    local termwidth=$((COLUMNS))          # Total available space.
-    local threshold=$((0.8 * termwidth)) # Threshold for switching to a
-                                         # multi-line prompt.
-
-    # Compute the length of the components.
-    local dlen llen blen vlen rlen;
-    # Length of the path.
-    dlen=${#:-(${(%)prt_dir})}
-    # Total length of the left prompt
-    (( llen = ${#:-[${(%)prt_user} ]# } + ${#prt_venv} + dlen ))
-
-    blen=${#${(S%%)col_bat//$~zero/}}             # Battery
-    vlen=${#${(S%%)col_vcs//$~zero/}}             # Version control.
-    (( rlen = ${#${(%)prt_jobs}} + vlen + blen )) # Right prompt.
-
-    # Check if we can put everything on a single line.
-    if [[ $((llen + rlen <= threshold)) -ne 0 ]]; then
-        PROMPT="${col_ret}[$col_user $col_dir]${prt_venv}$col_char "
-        RPROMPT="$col_vcs$col_bat$prt_jobs"
-        return
-    fi
-
-    # We have a multi-line prompt. The contents of RPROMPT would have been
-    # placed on the same line as the last character in PROMPT, so we add its
-    # contents to PROMPT instead, after some padding.
-    unset RPROMPT
-
-    # This will be the left part of the second line.
-    local snd_prt="
-${col_ret}[$col_user]${prt_venv}$col_char "
-
-
-    # Put PWD and what used to be RPROMPT on the same line as long as it has
-    # space for both.
-    if [[ $(((dlen + rlen) < termwidth)) -ne 0 ]]; then
-        # Add the path specifier to the prompt.
-        PROMPT="($col_dir)"
-
-        # Distance between the left and right sides.
-        local pad=$((termwidth - (dlen + rlen)))
-
-        PROMPT+="%{%B%F{black}%}${(l:((pad))::-:):-}%{%b%f%}"
-        PROMPT+="$col_vcs$col_bat$prt_jobs"
-        PROMPT+="$snd_prt"
-    fi
+# Prevent the display of child processes from going blank when a child process
+# ends, while still taking up space.
+TRAPCHLD() {
+    [[ -o notify ]] && zle && { zle reset-prompt; zle -R }
 }
